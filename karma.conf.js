a2831f9542ce815d370af1485895da1e58bb6068
Mariusz Sielski <mariusz.sielski@camunda.com>
2016-11-28 10:50:26 +0100

    add travis config

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 var webpackConf = require('./webpack.config-test');
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -69,6 +69,20 @@ module.exports = function(config) {
       // webpack-dev-middleware configuration
       // i. e.
       stats: 'errors-only'
+    },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     }
-  });
+  };
+
+  if(process.env.TRAVIS){
+    console.log(process.env.TRAVIS);
+
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
